50f9a5f59d31b8dc7fd8c1f85d2b6f222a7b2b85
Hara Kang <hara0115@gmail.com>
2013-05-23 19:43:29 -0700

    changed ingredient default order

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,7 @@
 class Ingredient < ActiveRecord::Base
   # attr_accessible :title, :body
+  default_scope order('name')
+
   has_many :recipe_relationships, :class_name => "RecipeIngredient", :foreign_key => "recipe_id"
   has_many :recipes, :through => :recipe_relationships, :source => :recipe
   attr_accessible :name, :image
