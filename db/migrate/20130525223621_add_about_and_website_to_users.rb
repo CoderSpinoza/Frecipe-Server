fc14978c0c7d227eb2b949b7556e00e09e05b02b
Hara Kang <hara0115@gmail.com>
2013-05-28 13:24:42 -0700

    there's something wrong with routes

--- db/migrate/20130525223621_add_about_and_website_to_users.rb
+++ db/migrate/20130525223621_add_about_and_website_to_users.rb
@@ -1,6 +1,6 @@
 class AddAboutAndWebsiteToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :about, :text
-    add_column :users, :website, :string
+    add_column :users, :about, :text, :default => ""
+    add_column :users, :website, :string, :default => ""
   end
 end
