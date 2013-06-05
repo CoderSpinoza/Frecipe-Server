38b7d3ade0edc18939c964472d74bc5e100f200b
Hara Kang <hara0115@gmail.com>
2013-06-04 23:49:52 -0700

    changed email alread exists error message

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,36 +6,37 @@ class RegistrationsController < Devise::RegistrationsController
       respond_to do |format|
         format.json { render :json => { :message => "An account with the email you entered already exists."} }
       end
-    end
-    if resource.save
-      if resource.active_for_authentication?
-        sign_up(resource_name, resource)
-        # session = UserSession.
-        resource.ensure_authentication_token!
-        session = UserSession.new(:user => resource)
+    else
+      if resource.save
+        if resource.active_for_authentication?
+          sign_up(resource_name, resource)
+          # session = UserSession.
+          resource.ensure_authentication_token!
+          session = UserSession.new(:user => resource)
 
-        if session.save
-          session.ensure_authentication_token!
-          grocery_recipe = GroceryRecipe.create(:user => resource, :recipe_id => 0)
-          UserMailer.welcome(resource).deliver
-          respond_to do |format|
-            format.json { render :json => {:message => "success", :user => resource, :authentication_token => session.authentication_token, :profile_picture => resource.profile_picture.url}}
+          if session.save
+            session.ensure_authentication_token!
+            grocery_recipe = GroceryRecipe.create(:user => resource, :recipe_id => 0)
+            UserMailer.welcome(resource).deliver
+            respond_to do |format|
+              format.json { render :json => {:message => "success", :user => resource, :authentication_token => session.authentication_token, :profile_picture => resource.profile_picture.url}}
+            end
+          else
+            respond_to do |format|
+              format.json { render :json => {:message => "session creation failed", :user => resource}, :status => 400}
+            end
           end
         else
+          expire_session_data_after_sign_in!
           respond_to do |format|
-            format.json { render :json => {:message => "session creation failed", :user => resource}, :status => 400}
+            format.json { render :json => { :message => "inactive", :user => resource }, :status => 400 }
           end
         end
       else
-        expire_session_data_after_sign_in!
+        clean_up_passwords resource
         respond_to do |format|
-          format.json { render :json => { :message => "inactive", :user => resource }, :status => 400 }
+          format.json { render :json => { :message => "error", :user => resource }, :status => 400 }
         end
-      end
-    else
-      clean_up_passwords resource
-      respond_to do |format|
-        format.json { render :json => { :message => "error", :user => resource }, :status => 400 }
       end
     end
 	end
