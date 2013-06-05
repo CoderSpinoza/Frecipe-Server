8bf363073c09e95a59d3b09075b1e0539c0c2ce6
Hara Kang <hara0115@gmail.com>
2013-06-04 23:45:29 -0700

    finished implementing signup error messages

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,11 @@
 class RegistrationsController < Devise::RegistrationsController
 	def create
 		build_resource
-    uid = params[:uid]
-    if uid
-      # resource.provider = 'facebook'
-      # resource.uid = uid
+
+    if User.find_by_email(resource[:email])
+      respond_to do |format|
+        format.json { render :json => { :message => "An account with the email you entered already exists."} }
+      end
     end
     if resource.save
       if resource.active_for_authentication?
@@ -18,23 +19,23 @@ class RegistrationsController < Devise::RegistrationsController
           grocery_recipe = GroceryRecipe.create(:user => resource, :recipe_id => 0)
           UserMailer.welcome(resource).deliver
           respond_to do |format|
-            format.json { render :json => ["success", resource, session.authentication_token, resource.profile_picture.url ]}
+            format.json { render :json => {:message => "success", :user => resource, :authentication_token => session.authentication_token, :profile_picture => resource.profile_picture.url}}
           end
         else
           respond_to do |format|
-            format.json { render :json => ["session creation failed", resource], :status => 400}
+            format.json { render :json => {:message => "session creation failed", :user => resource}, :status => 400}
           end
         end
       else
         expire_session_data_after_sign_in!
         respond_to do |format|
-          format.json { render :json => ["inactive", resource], :status => 400 }
+          format.json { render :json => { :message => "inactive", :user => resource }, :status => 400 }
         end
       end
     else
       clean_up_passwords resource
       respond_to do |format|
-        format.json { render :json => ["error", resource], :status => 400 }
+        format.json { render :json => { :message => "error", :user => resource }, :status => 400 }
       end
     end
 	end
