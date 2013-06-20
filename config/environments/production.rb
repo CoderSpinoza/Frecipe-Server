ad478f673d1771df724ab5daf14f04d74bc7ff6d
Hara Kang <hara0115@gmail.com>
2013-06-19 17:08:05 -0700

    removed memcahier stuff from production.rb. Would it work for signing in?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,9 +40,7 @@ Frecipe::Application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production
-  config.cache_store = :dalli_store, ENV["MEMCACHIER_SERVERS"],
-                    {:username => ENV["MEMCACHIER_USERNAME"],
-                     :password => ENV["MEMCACHIER_PASSWORD"]}
+  config.cache_store = :dalli_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
