e2b71ddd44a340eb66d29a59710397c57f4b774e
Hara Kang <hara0115@gmail.com>
2013-06-24 16:24:44 -0700

    changed ingredient image size in /ingredients

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,7 @@ Frecipe::Application.configure do
     :storage => :s3,
     :bucket => 'FrecipeProduction',
     :s3_credentials => {
-      :access_key_id => ENV['S3_KEY'],
+      :access_key_id => ENV['S3_KEY']
       :secret_access_key => ENV['S3_SECRET']
     }
   })
