63cc17358b2fbf8881f85e71043850c62d4911a7
Hara Kang <hara0115@gmail.com>
2013-07-13 02:47:19 -0700

    let's do autocomplete

--- db/migrate/20130712084746_add_total_points_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddTotalPointsToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :total_points, :integer
-  end
-end
--- db/migrate/20130712085309_set_default_value_for_total_points_in_users.rb
+++
@@ -1,7 +0,0 @@
-class SetDefaultValueForTotalPointsInUsers < ActiveRecord::Migration
-  def up
-  end
-
-  def down
-  end
-end
