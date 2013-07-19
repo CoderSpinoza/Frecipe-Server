ed93008ebd3b82f9891883291d4bf4881ee2e82c
Hara Kang <hara0115@gmail.com>
2013-07-18 17:02:33 -0700

    There was a typo in rankings controller. Fixed it

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -2,7 +2,7 @@ class RankingsController < ApplicationController
 	def index
 		@users = User.all
 		respond_to do |format|
-			format.json { :json => {:users => @users, :message => "success"}}
+			format.json { render :json => {:users => @users, :message => "success"}}
 		end
 	end
 end
