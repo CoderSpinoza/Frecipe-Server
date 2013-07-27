107278d533cfd21ec505ecce19791ac7536f1454
Hara Kang <hara0115@gmail.com>
2013-07-26 20:35:26 -0700

    pushing event information in rankings

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -4,7 +4,7 @@ class RankingsController < ApplicationController
 		@event = Event.first
 		@user = UserSession.user_by_authentication_token(params[:authentication_token])
 		respond_to do |format|
-			format.json { render :json => {:users => @users, :user => @user, :event => @event, message => "success"}}
+			format.json { render :json => {:users => @users, :user => @user, :event => @event, :message => "success"}}
 		end
 	end
 end
